87f691dbca86fc1273f047954393797c5ebd29e0
Sourav Das <sourav.das01@sap.com>
2022-02-13 20:31:50 +0530

    converted yaml to js-forge build

--- src/yaml/interfaces/Command.ts
+++
@@ -1,6 +0,0 @@
-import {Step} from "../models/BuildScript";
-import {Context} from "../../interfaces/Context";
-
-export interface Command {
-    process(context: Context, step: Step): Promise<void>;
-}
